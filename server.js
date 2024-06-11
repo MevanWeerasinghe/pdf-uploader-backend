f1dedd2855195bdca1835b33f280d84ae7e9bc6b
Mevan Weerasinghe <marvolotrigg@gmail.com>
2024-06-11 15:21:31 +0530

    DB connection

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
-const app = require("./app");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const morgan = require("morgan");
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+const pdfRoutes = require("./routes/pdfRoutes");
+const logger = require("./middleware/logger");
+const connectDB = require("./config/dbConnection");
+
+connectDB();
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(morgan("combined", { stream: logger.stream }));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/pdfs", pdfRoutes);
 
 const port = process.env.PORT || 5000;
 
